94859360d4b2010aeb4270e49f34d1f8440c0cef
Ryne Emory <ryneemory@gmail.com>
2015-08-22 14:17:38 -0600

    place order button

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -2,6 +2,16 @@ app.controller("getTeas", function($scope, $http) {
   $scope.totalItems = 0;
   $scope.cartEdit = false;
   $scope.cart = [];
+  $scope.total = 0;
+
+  $scope.orderTotal = function () {
+    $scope.total = 0;
+    for (var i = 0; i < $scope.cart.length; i++) {
+      var item = $scope.cart[i];
+      $scope.total += item.quantity * item.price
+    }
+  }
+
   $http.get('teaData.json').then(function(data) {
     $scope.teaData = data.data;
     $scope.teaData.forEach(function(tea) {
@@ -9,21 +19,42 @@ app.controller("getTeas", function($scope, $http) {
       tea.quantity = 0;
     })
   })
+
   $scope.addToBag = function(tea, quantity) {
+    console.log(this.tea.$$hashKey);
+    //need to handle duplicates to match the hashkey
     if (quantity === undefined) {
       quantity = 1;
     }
     tea.quantity = quantity;
     $scope.totalItems = $scope.totalItems + parseInt(quantity)
     $scope.cart.push(tea)
+    $scope.orderTotal();
   }
-
 
   $scope.save = function(newQuantity) {
     this.cartEdit = false;
+    $scope.totalItems = $scope.totalItems - parseInt(this.tea.quantity);
+    $scope.totalItems = $scope.totalItems + newQuantity;
     this.tea.quantity = newQuantity;
+    $scope.orderTotal();
   }
 
-// app.controller("cart", function() {
-//
+  $scope.remove = function () {
+    for (i=0; i < $scope.cart.length; i++) {
+      var x = $scope.cart[i]
+      if (x._id === this.tea._id) {
+        $scope.cart.splice(i, 1)
+        $scope.totalItems = $scope.totalItems - parseInt(this.tea.quantity);
+      }
+    }
+    $scope.orderTotal();
+  }
+
+  $scope.clearCart = function () {
+    $scope.cart = [];
+    $scope.total = 0;
+    $scope.totalItems = 0;
+  }
+
  })
