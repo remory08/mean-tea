d1793e29185d585786d222eb5779d6f75dffeb5f
Ryne Emory <ryneemory@gmail.com>
2015-08-22 13:11:47 -0600

    #101827798 CP = list each item that was added  from the previous page, and includes its quantity

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,16 @@
-var app = angular.module("meanTea", []);
+var app = angular.module("meanTea", ['ngRoute']);
+
+function configFn($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/allTeas.html',
+    })
+    .when('/checkout', {
+      templateUrl: 'partials/shoppingCart.html',
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+}
+
+app.config(  ['$routeProvider', '$locationProvider', configFn])
--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -1,12 +1,25 @@
 app.controller("getTeas", function($scope, $http) {
+  $scope.totalItems = 0;
+  $scope.cart = [];
   $http.get('teaData.json').then(function(data) {
     $scope.teaData = data.data;
     $scope.teaData.forEach(function(tea) {
       tea.price = tea.price/100;
+      tea.quantity = 0;
     })
   })
+  $scope.addToBag = function(tea, quantity) {
+    if (quantity === undefined) {
+      quantity = 1;
+    }
+    tea.quantity = quantity;
+    $scope.totalItems = $scope.totalItems + parseInt(quantity)
+    console.log(tea);
+    $scope.cart.push(tea)
+    console.log($scope.cart);
+  }
 })
 
-app.controller("cart", function() {
-  
-})
+// app.controller("cart", function() {
+//
+// })
